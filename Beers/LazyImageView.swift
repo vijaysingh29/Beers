5a939220a9b792b5a51153a21bf6d4fa4df17b77
vijaysingh29 <vijaygangwar29@gmail.com>
2021-09-26 12:49:40 +0530

    LazyImageView
    image cache clear

---
+++ Beers/LazyImageView.swift
@@ -0,0 +1,42 @@
+//
+//  LazyImageView.swift
+//  Beers
+//
+//  Created by Vijay B. Singh on 25/09/21.
+//
+
+import Foundation
+import UIKit
+
+class LazyImageView: UIImageView {
+    private let imageCache = NSCache<AnyObject, UIImage>()
+
+    func loadImage(fromURL imageURL: URL, placeHolderImage: String)
+    {
+        self.image = UIImage(named: placeHolderImage)
+
+        if let cachedImage = self.imageCache.object(forKey: imageURL as AnyObject)
+        {
+           // debugPrint("image loaded from cache for =\(imageURL)")
+            self.image = cachedImage
+            return
+        }
+
+        DispatchQueue.global().async {
+            [weak self] in
+
+            if let imageData = try? Data(contentsOf: imageURL)
+            {
+              //  debugPrint("image downloaded from server...")
+                if let image = UIImage(data: imageData)
+                {
+                    DispatchQueue.main.async {
+                        self!.imageCache.setObject(image, forKey: imageURL as AnyObject)
+                        self?.image = image
+                    }
+                }
+            }
+        }
+        imageCache.removeObject(forKey:  imageURL as AnyObject)
+    }
+}
